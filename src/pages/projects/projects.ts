a1df6df9232a43155971efafe5f8f2299c1af1e7
estellel-github <131867732+estellel-github@users.noreply.github.com>
2025-02-03 09:30:52 +0100

    Shorten project descs

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -13,40 +13,40 @@ export interface Project {
 export const projects: Project[] = [
   {
     name: 'Estelle L. - Dev Portfolio',
-    description: 'An Astro dev portfolio and blog to post about my journey as a dev and my projects.',
+    description: 'An Astro dev portfolio and blog.',
     repoLink: 'https://github.com/estellel-github/dev-portfolio',
     tags: ['Blog', 'Astro']
   },
   {
     name: 'Movie Club App Backend',
-    description: 'A backend app for managing a movie club (capstone full-stack project - in progress).',
+    description: 'A backend app for a movie club (in progress).',
     repoLink: 'https://github.com/estellel-github/movie-club-app',
     tags: ['TS', 'Node', 'Express', 'PSQL']
   },
   {
     name: 'Node-Express Book App',
-    description: 'A basic RESTful API built with Node.js and Express.',
+    description: 'A basic RESTful API with Node.js & Express.',
     repoLink: 'https://github.com/estellel-github/node-express-basic-app',
     repoLinkRel: 'nofollow noopener noreferrer',
     tags: ['Node', 'Express', 'JS']
   },
   {
     name: 'To Do App',
-    description: 'A To Do app built with JavaScript/TypeScript and Webpack.',
+    description: 'A To Do app with TypeScript and Webpack.',
     repoLink: 'https://github.com/estellel-github/todo-app-webpack',
     repoLinkRel: 'nofollow noopener noreferrer',
     tags: ['TS', 'Webpack']
   },
   {
     name: 'Calculator',
-    description: 'A simple and elegant calculator app built using HTML, CSS, and JavaScript.',
+    description: 'A calculator app with HTML, CSS, JS.',
     repoLink: 'https://github.com/estellel-github/calculator',
     repoLinkRel: 'nofollow noopener noreferrer',
     tags: ['JS']
   },
   {
     name: 'Etch-A-Sketch',
-    description: 'A digital Etch-a-Sketch app built with HTML, CSS, and JavaScript.',
+    description: 'A digital Etch-a-Sketch app with HTML, CSS, JS.',
     repoLink: 'https://github.com/estellel-github/etch-a-sketch',
     tags: ['JS']
   }
